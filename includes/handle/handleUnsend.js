59c3198dbdb4c726505a10128e40932c298504ae
shelbyonex <149349981+shelbyonex@users.noreply.github.com>
2023-12-10 21:39:44 +0700

    Add files via upload

---
+++ includes/handle/handleUnsend.js
@@ -0,0 +1,9 @@
+module.exports = function({ api }) {
+   const reaction = "😠";
+     return function({ event }) {
+   const { senderID, reaction, messageID } = event;
+   if (senderID == api.getCurrentUserID()) {
+   if (reaction == "😠") return api.unsendMessage(messageID);
+   }
+     };
+  };
